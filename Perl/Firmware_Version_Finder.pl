523437839782f7b72bf608d97e0c022c02904386
M. Moccaro <mfm2772@comcast.net>
2017-03-04 22:05:28 -0500

    Update Firmware_Version_Finder.pl

--- Perl/Firmware_Version_Finder.pl
+++ Perl/Firmware_Version_Finder.pl
@@ -1,28 +1,42 @@
 #!/usr/bin/env perl 
 
+#  File: Firmware_Version_Finder.pl
+#  Type: Perl Source File 
+#  Date: March 4, 2017 
+# 
+#  Description: 
+#  THis file logs into a network device via SSH and print the firmware
+#   version to the screen.
+
+#Pragmas
 use Net::SSH::Expect;
 
+#Create an SSH session
 my $ssh = Net::SSH::Expect->new (
-    host => "127.0.0.1", 
+    host => "10.0.0.17", 
     user => 'user',
     password=> 'user_password',  
     raw_pty => 1,
     no_terminal => 0
 );
 
-my $login_info = $ssh->login();
-print "Login output: $login_info\n";
+#Login
+my $login_output = $ssh->login();
 
+#Change Modes
 my $command = $ssh->exec("en");
-print("$command\n");
 
+#Enter a password
 $ssh->waitfor("Password:", 1);
-$ssh->send("secret_password");	
+$ssh->send("secret_password");
 
+#Run this command to receive clean output
 $command = $ssh->exec("terminal length 0");
-print("$command\n");
 
+#Run the version command and
+#display the output.
 $command = $ssh->exec("sh ver");
-print("$command\n");
+print "$command\n";
 
+#Close the connection
 $ssh->close();
