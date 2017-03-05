0dd3bec099e4459b62386a26bd51f6febce85b61
M. Moccaro <mfm2772@comcast.net>
2017-03-04 22:09:18 -0500

    Create Reload.py

---
+++ Python/Reload.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python 
+
+#  File: Reload.py
+#  Type: Python Source File 
+#  Date: March 4, 2017 
+# 
+#  Description: 
+#  This file logs into a network device via SSH and reloads it.
